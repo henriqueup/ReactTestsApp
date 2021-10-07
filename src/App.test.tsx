ed8d09b2d898d6901f66a05944545f5649424601
Henrique Pessoa <henrique.pessoa@dtidigital.com.br>
2021-10-07 11:57:10 -0300

    implement initial table test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { data } from './tableMock';
+
+beforeEach(() => render(<App />))
 
 test('renders learn react link', () => {
-  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders lemons table initially with every instance', () => {
+  const tableWrapper = screen.getByRole('table');
+  expect(tableWrapper).toBeInTheDocument();
+
+  const rows = screen.getAllByRole('row');
+  expect(rows.length).toBe(data.length + 1);
+})
